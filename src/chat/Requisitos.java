e4079f6a819b3cfd92d35b6ab216495b77f02616
Sagacius430 <lincolncefet@gmail.com>
2019-10-31 22:00:17 -0200

    Alteração do Requisitos.java

--- src/chat/Requisitos.java
+++ src/chat/Requisitos.java
@@ -49,8 +49,36 @@ transmitir:João:Maria:Olá Maria, como vai? Aqui é o João
 transmitir:Maria:João;José:Vocês terminaram o trabalho de LP2?
 transmitir:*:Bom dia para todos!
 * O chat irá operar na porta 2424 (escolha do Prof. Ederson !)
-
 Última atualização: quinta, 3 Out 2019, 19:38
     */
     
+    
+    /*****************************quinta, 7 Nov 2019, 22:30****************
+ /**
+  CHAT - Entrega Final (Servidor) - Mensagem/Transmitir/Sair
+Implemente os seguintes métodos do protocolo:
+
+mensagem:destinatário:texto da mensagem
+Mensagem enviada do cliente para o servidor quando este desejar enviar uma mensagem para os outros usuários.
+O parâmetro destinatário pode ser um nome simples, uma lista de nomes separados por ponto e vírgula ou asterisco caso a mensagem deva ser enviada para todos os usuários.
+O servidor não deve responder essa mensagem.
+Exemplos:
+mensagem:João:Bom dia
+mensagem:João;Maria:Olá
+mensagem:*:Bom dia galera!
+transmitir:remetente:destinatário:texto da mensagem
+Mensagem enviada do servidor para o cliente quando o cliente é um dos destinatários da mensagem.
+O parâmetro remetente é o nome do remetente
+O parâmetro destinatário pode ser o nome de um destinatário, uma lista de destinatários separada por ponto e vírgula ou asterisco caso a mensagem seja destinada para todos os usuários.
+O cliente não deve responder essa mensagem
+Exemplos:
+transmitir:João:Maria:Olá Maria, como vai? Aqui é o João
+transmitir:Maria:João;José:Vocês terminaram o trabalho de LP2?
+transmitir:*:Bom dia para todos!
+sair
+Mensagem enviada do cliente para o servidor
+Ao receber essa mensagem o servidor deve desconectar o cliente.
+Quando o cliente for desconectado a lista de usuário deve ser atualizada e enviada a todos os demais clientes conectados.
+  
+  */   
 }
